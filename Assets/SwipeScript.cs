4c3b9936fdd352a6c9b0c31b065e60e00d3ef03c
magelon <alfardproject@gmail.com>
2019-04-14 12:48:10 -0700

    c

--- Assets/SwipeScript.cs
+++ Assets/SwipeScript.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class SwipeScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    Vector2 startPos, endPos, direction;
+    float touchTimeStart, touchTimeFinish, timeInterval;
 
-    // Update is called once per frame
+    [Range(0.05f, 1f)]
+    public float throwForse = 0.3f;
+
+    
     void Update()
     {
-        
+        //if you touch the screen
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            //getting touch position and marking time when tou touch the screen
+            touchTimeStart = Time.time;
+            startPos = Input.GetTouch(0).position;
+        }
+        //if you release your finger
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended)
+        {
+            //marking time when you release it
+            touchTimeFinish = Time.time;
+            //calculate swipe time interval
+            timeInterval = touchTimeFinish - touchTimeStart;
+            //getting release finger position
+            endPos = Input.GetTouch(0).position;
+            //calculating swipe direction
+            direction = -(startPos - endPos);
+            //add force to ball rigidbody depending on swipe time and direction
+            GetComponent<Rigidbody2D>().AddForce(direction / timeInterval * throwForse);
+        }
     }
 }
