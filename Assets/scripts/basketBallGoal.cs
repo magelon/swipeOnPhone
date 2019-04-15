56f0e93b6fa2607830d928df45a2c9812c5d9dbd
magelon <alfardproject@gmail.com>
2019-04-14 23:04:55 -0700

    c

---
+++ Assets/scripts/basketBallGoal.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class basketBallGoal : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.name == "ball")
+        {
+            Debug.Log("goal!");
+        }
+    }
+}
