9f4f80f911013b23c7d7b70a6ebcbcdda617ef06
lon <alfardproject@gmail.com>
2019-04-16 09:03:17 -0700

    c

--- Assets/scripts/basketBallGoal.cs
+++ Assets/scripts/basketBallGoal.cs
@@ -4,11 +4,22 @@ using UnityEngine;
 
 public class basketBallGoal : MonoBehaviour
 {
+    public GameObject win;
+    private ParticleSystem p;
+    private void Start()
+    {
+        p = win.GetComponent<ParticleSystem>();
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.name == "ball")
         {
             Debug.Log("goal!");
+            if (!p.isPlaying)
+            {
+                p.Play();
+            }
+
         }
     }
 }
